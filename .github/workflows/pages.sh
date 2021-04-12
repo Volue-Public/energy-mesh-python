9a84b70dee6c075d39fa61015fd2fc45394bfc47
Erik Nyhus <erny@powel.com>
2021-04-12 13:53:53 +0200

    Add GitHub action to publish docs to gh pages
    This is run automatically on every to push to master to keep our online
    docs up to date.

---
+++ .github/workflows/pages.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Build the library documentation and push it to GitHub pages.
+#
+# Usage: pages.sh <repo> <gh-pages>
+#
+# <repo>        A directory containing the sources from which the documentation
+#               will be generated. In most cases this should be an up to date
+#               checkout from `origin/master`.
+#
+# <gh-pages>    A directory containing a checkout with the `origin/gh-pages`
+#               branch.
+#
+# See also `pages.yml`, which is the primary consumer of this script.
+
+set -o errexit # Fail the script if a command returns non-zero.
+set -o nounset # Fail the script if an unset variable is used.
+
+repo_dir=$1
+pages_dir=$2
+
+make -C "$repo_dir/docs" html
+rm -rf "$pages_dir/"*
+cp -r "$repo_dir/docs/build/html/"* "$pages_dir/"
+
+# By default GitHub pages treats a site like a Jekyll page and uses Jekyll to
+# build the page. Normally this isn't a problem for purely static content as
+# that's (mostly) a valid Jekyll page. Jekyll does however ignore files with
+# leading underscores, and Sphinx generates directories like `_static/`.
+#
+# Adding a `.nojekyll` file make GitHub treat the page as purely static files.
+touch "$pages_dir/.nojekyll"
+
+git -C "$pages_dir" commit -m "Update GitHub pages" --allow-empty
+git -C "$pages_dir" push
