5a548e0ef8b90356e53038d8fb203331023e6f5e
Erik Nyhus <erny@powel.com>
2021-04-26 12:02:58 +0200

    Bump version to 0.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Volue AS'
 author = 'Volue AS'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
