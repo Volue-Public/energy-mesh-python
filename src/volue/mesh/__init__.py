5912fab03f2ec2d64261923e36613fbc5b048dd9
Pål Davik <pada@powel.com>
2021-05-04 09:22:53 +0200

    Use TLS/SSL

--- src/volue/mesh/__init__.py
+++ src/volue/mesh/__init__.py
@@ -2,6 +2,7 @@
 
 __version__ = "0.1.0"
 
+import os
 import grpc
 
 from volue.mesh.proto import mesh_pb2
@@ -9,9 +10,25 @@ from volue.mesh.proto import mesh_pb2_grpc
 from google import protobuf
 
 
+class Credentials:
+    def __init__(self):
+        self.server_cert_path = os.path.join(
+            os.path.dirname(__file__),
+            'cert/server_self_signed_crt.pem')
+
+        with open(self.server_cert_path, 'rb') as file:
+            self.server_cert = file.read()
+
+        self.channel_creds = grpc.ssl_channel_credentials(
+            root_certificates=self.server_cert)
+
+
 class AsyncConnection:
     def __init__(self):
-        self.channel = grpc.aio.insecure_channel("localhost:50051")
+        self.credentials = Credentials()
+        self.channel = grpc.aio.secure_channel(
+            "localhost:50051",
+            self.credentials.channel_creds)
         self.stub = mesh_pb2_grpc.MeshServiceStub(self.channel)
 
     async def get_version(self):
@@ -27,7 +44,10 @@ class AsyncConnection:
 
 class Connection:
     def __init__(self):
-        self.channel = grpc.insecure_channel('localhost:50051')
+        self.credentials = Credentials()
+        self.channel = grpc.secure_channel(
+            'localhost:50051',
+            self.credentials.channel_creds)
         self.stub = mesh_pb2_grpc.MeshServiceStub(self.channel)
 
     def get_version(self):
