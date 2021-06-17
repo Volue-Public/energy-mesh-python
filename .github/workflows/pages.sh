8bdfa70df64e3b426a40779aba88d752c6cbbfd6
Janne Beate Bakeng <janne.beate.bakeng@volue.com>
2021-06-17 14:40:36 +0200

    Fixing path issue.

--- .github/workflows/pages.sh
+++ .github/workflows/pages.sh
@@ -20,8 +20,7 @@ repo_dir=$1
 pages_dir=$2
 
 source "$HOME/.poetry/env"
-cd "$repo_dir"
-poetry -v run "$repo_dir/src/docs/make html"
+poetry -v run "$repo_dir/docs/make html"
 rm -rf "${pages_dir:?}/"*
 cp -r "$repo_dir/docs/build/html/"* "$pages_dir/"
 
