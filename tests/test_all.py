01a00ff032338b428f959922f4159644b1e604b2
Janne Beate Bakeng <janne.beate.bakeng@volue.com>
2021-08-19 11:09:16 +0200

    Readded timesseries tests to aøll tests.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,12 +1,13 @@
 import glob, os
 from test_session import *
+from test_timeseries import *
 
 
 class RunExamples(unittest.TestCase):
 
     def test_run_example_scripts(self):
         """
-        Run all example script and check if they exit with no error code.
+        Run all example scripts and check if they exit with no error code.
         """
         os.chdir("../examples/mesh/")
         for file in glob.glob("*.py"):
