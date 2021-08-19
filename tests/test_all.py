3ec14b3552505c55bf4867113e2c52c8f6a2e6d2
Janne Beate Bakeng <janne.beate.bakeng@volue.com>
2021-08-19 10:28:41 +0200

    Example scripts are now automatically a part of tests when added to the examples/mesh folder.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,28 +1,16 @@
-import unittest
-
-from test_utilities import *
-from test_timeseries import *
+import glob, os
 from test_session import *
 
 
 class RunExamples(unittest.TestCase):
 
-    # TODO Automate generation of example script running
-
-    def test_1_get_version(self):
-        run_example_script(self, "../examples/mesh/1_get_version.py")
-
-    def test_2a_get_timeseries_points(self):
-        run_example_script(self, "../examples/mesh/2a_get_timeseries_points.py")
-
-    def test_2b_get_timeseries_points_async(self):
-        run_example_script(self, "../examples/mesh/2b_get_timeseries_points_async.py")
-
-    def test_3a_edit_timeseries_points(self):
-        run_example_script(self, "../examples/mesh/3a_edit_timeseries_points.py")
-
-    def test_3b_edit_timeseries_points_async(self):
-        run_example_script(self, "../examples/mesh/3b_edit_timeseries_points_async.py")
+    def test_run_example_scripts(self):
+        """
+        Run all example script and check if they exit with no error code.
+        """
+        os.chdir("../examples/mesh/")
+        for file in glob.glob("*.py"):
+            run_example_script(self, file)
 
 
 if __name__ == '__main__':
