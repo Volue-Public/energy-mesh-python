0d16aaaa90b04ea9a99109b219a63da47ab0b656
Janne Beate Bakeng <janne.beate.bakeng@volue.com>
2021-09-30 21:07:34 +0200

    Removed unused _static folder.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,7 +99,7 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme_options = {
     'logo_only': True,
