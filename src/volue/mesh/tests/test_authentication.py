619f52a99fc94d86ddffe68d9c81f31aa747480c
Janne Beate Bakeng <436265+jbbakeng@users.noreply.github.com>
2022-01-17 10:53:56 +0100

    Fixing error in documentation build. (#105)

--- src/volue/mesh/tests/test_authentication.py
+++ src/volue/mesh/tests/test_authentication.py
@@ -4,11 +4,11 @@ from volue.mesh import Authentication, Connection, Credentials
 from volue.mesh.aio import Connection as AsyncConnection
 import volue.mesh.tests.test_utilities.server_config as sc
 
+
 @pytest.fixture
 def auth_metadata_plugin():
     """Yields Authentication object. No clean-up is done (it requires Connection object)
-    Note: Depending on the test-case there will be some tokens left
-          (not revoked) in Mesh server."""
+    Note: Depending on the test-case there will be some tokens left (not revoked) in Mesh server."""
     target = f'{sc.DefaultServerConfig.ADDRESS}:{sc.DefaultServerConfig.PORT}'
     credentials = Credentials()
     authentication_parameters = Authentication.Parameters(
@@ -22,9 +22,9 @@ def connection():
     authentication_parameters = Authentication.Parameters(
         sc.DefaultServerConfig.KERBEROS_SERVICE_PRINCIPAL_NAME)
     conn = Connection(sc.DefaultServerConfig.ADDRESS,
-        sc.DefaultServerConfig.PORT,
-        True,  # authentication requires TLS (to make sure tokens are encrypted)
-        authentication_parameters)
+                      sc.DefaultServerConfig.PORT,
+                      True,  # authentication requires TLS (to make sure tokens are encrypted)
+                      authentication_parameters)
     yield conn
 
     # clean-up
@@ -37,9 +37,9 @@ async def aconnection():
     authentication_parameters = Authentication.Parameters(
         sc.DefaultServerConfig.KERBEROS_SERVICE_PRINCIPAL_NAME)
     aconn = AsyncConnection(sc.DefaultServerConfig.ADDRESS,
-        sc.DefaultServerConfig.PORT,
-        True,  # authentication requires TLS (to make sure tokens are encrypted)
-        authentication_parameters)
+                            sc.DefaultServerConfig.PORT,
+                            True,  # authentication requires TLS (to make sure tokens are encrypted)
+                            authentication_parameters)
     yield aconn
 
     # clean-up
@@ -53,6 +53,7 @@ def test_auth_metadata_plugin_obtains_valid_token_in_init(auth_metadata_plugin):
     assert auth_metadata_plugin.token_expiration_date is not None
     assert auth_metadata_plugin.is_token_valid()
 
+
 @pytest.mark.authentication
 def test_delete_access_token(auth_metadata_plugin):
     """Check if deleting access token correctly resets token class members."""
@@ -60,11 +61,13 @@ def test_delete_access_token(auth_metadata_plugin):
     assert auth_metadata_plugin.token is None
     assert auth_metadata_plugin.token_expiration_date is None
 
+
 @pytest.mark.authentication
 def test_is_valid_token_returns_false_for_deleted_access_token(auth_metadata_plugin):
     """Check if after deleting access token the validation method will return False."""
     auth_metadata_plugin.delete_access_token()
     assert auth_metadata_plugin.is_token_valid() is False
+
 
 @pytest.mark.authentication
 def test_auth_metadata_plugin_obtains_correctly_new_token_after_delete(auth_metadata_plugin):
@@ -83,12 +86,14 @@ def test_auth_metadata_plugin_obtains_correctly_new_token_after_delete(auth_meta
     assert new_token is not previous_token
     assert previous_token_expiration_date < new_token_expiration_date
 
+
 @pytest.mark.authentication
 def test_connection_revoke_access_token(connection):
     """Check if revoking access token from Connection class correctly invalidates it."""
     assert connection.auth_metadata_plugin.is_token_valid()
     connection.revoke_access_token()
     assert connection.auth_metadata_plugin.is_token_valid() is False
+
 
 @pytest.mark.asyncio
 @pytest.mark.authentication
@@ -98,12 +103,14 @@ async def test_async_connection_revoke_access_token(aconnection):
     await aconnection.revoke_access_token()
     assert aconnection.auth_metadata_plugin.is_token_valid() is False
 
+
 @pytest.mark.authentication
 def test_connection_get_user_identity(connection):
     """Check if getting user identity from Connection class returns something."""
     user_identity = connection.get_user_identity()
     assert user_identity.display_name is not None
     assert user_identity.identifier is not None
+
 
 @pytest.mark.asyncio
 @pytest.mark.authentication
