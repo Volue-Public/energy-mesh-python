fe6a52f848cbe791e74d363bc594cb8c21fde18d
Janne Beate Bakeng <436265+jbbakeng@users.noreply.github.com>
2022-03-16 14:32:45 +0100

    Update src/volue/mesh/aio/_connection.py
    Co-authored-by: Tomasz Noczynski <88330807+tnoczyns-volue@users.noreply.github.com>

--- src/volue/mesh/aio/_connection.py
+++ src/volue/mesh/aio/_connection.py
@@ -429,7 +429,7 @@ class Connection:
                 end_time (datetime): |end_time|
 
             Returns:
-                StatisticalFunctions: object containing all statisical functions
+                StatisticalFunctions: object containing all statistical functions
             """
             return StatisticalFunctionsAsync(self, relative_to, start_time, end_time)
 
