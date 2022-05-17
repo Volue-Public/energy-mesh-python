b4b943af8f765b98cbf89cdcc4a2d264b996799b
Tomasz Noczynski <tomasz.noczynski@volue.com>
2022-05-17 19:09:23 +0200

    Remove import added by accident

--- src/volue/mesh/_attribute.py
+++ src/volue/mesh/_attribute.py
@@ -5,7 +5,6 @@ Functionality for working with Mesh attributes.
 from __future__ import annotations
 from dataclasses import dataclass, field
 from datetime import datetime
-from imp import SEARCH_ERROR
 from typing import Any, List, Type, TypeVar
 import uuid
 
