71727315e0ce1a3dee9c5254ea4dbf96f5b4e73e
Erik Nyhus <61583878+erny-powel@users.noreply.github.com>
2022-07-04 12:41:24 +0200

    xy: add example (#253)

---
+++ src/volue/mesh/examples/xy_sets.py
@@ -0,0 +1,50 @@
+import datetime
+
+from volue import mesh
+
+from volue.mesh.examples import _get_connection_info
+
+
+OBJECT_PATH = "Model/SimpleThermalTestModel/ThermalComponent.ThermalPowerToPlantRef/SomePowerPlant1"
+UNVERSIONED_PATH = OBJECT_PATH + ".XYSetAtt"
+VERSIONED_PATH = OBJECT_PATH + ".XYZSeriesAtt"
+SAMPLE_XY_SET = mesh.XySet(None,
+                           [mesh.XyCurve(0.0,
+                                         [(1.0, 10.3),
+                                          (2.5, 15.9)])])
+
+
+def main(address, port, tls_root_cert):
+    c = mesh.Connection(address, port, tls_root_cert)
+    with c.create_session() as session:
+        print(f"Getting XY-sets from {UNVERSIONED_PATH}")
+        xy_sets = session.get_xy_sets(UNVERSIONED_PATH)
+        print(xy_sets)
+
+        start_time = datetime.datetime.fromisoformat("1960-01-01")
+        end_time = datetime.datetime.fromisoformat("2030-01-01")
+        print(f"Getting XY-sets in interval [{start_time}, {end_time}) from {VERSIONED_PATH}")
+        xy_sets = session.get_xy_sets(VERSIONED_PATH, start_time, end_time)
+        print(f"Received: {xy_sets}")
+
+        print(f"Updating XY-set at {UNVERSIONED_PATH}")
+        session.update_xy_sets(UNVERSIONED_PATH, new_xy_sets=[SAMPLE_XY_SET])
+        print("Updated")
+
+        print(f"Replacing XY-set versions in interval [{start_time}, {end_time}) with one version at {start_time}")
+        new_xy_set = SAMPLE_XY_SET._replace(valid_from_time=start_time)
+        session.update_xy_sets(VERSIONED_PATH, start_time, end_time, [new_xy_set])
+        print("Updated")
+
+        print(f"Getting XY-sets from {UNVERSIONED_PATH}")
+        xy_sets = session.get_xy_sets(UNVERSIONED_PATH)
+        print(f"Received: {xy_sets}")
+
+        print(f"Getting XY-sets in interval [{start_time}, {end_time}) from {VERSIONED_PATH}")
+        xy_sets = session.get_xy_sets(VERSIONED_PATH, start_time, end_time)
+        print(f"Received: {xy_sets}")
+
+
+if __name__ == "__main__":
+    args = _get_connection_info()
+    main(*args)
