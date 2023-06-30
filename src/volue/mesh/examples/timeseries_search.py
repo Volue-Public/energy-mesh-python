8b0a1429bcdf45e2c502aaa13513f74da74c3d9b
Tomasz Noczynski <88330807+tnoczyns-volue@users.noreply.github.com>
2023-06-30 16:15:00 +0200

    Extend `timeseries_search` example (#357)
    * Extend `timeseries_search` example
    
    * Add more description
    
    * Add more comments

--- src/volue/mesh/examples/timeseries_search.py
+++ src/volue/mesh/examples/timeseries_search.py
@@ -1,20 +1,18 @@
 from volue.mesh.examples import _get_connection_info
-from volue.mesh import Connection
+from volue.mesh import Connection, OwnershipRelationAttribute, TimeseriesAttribute
 
 
-def main(address, port, root_pem_certificate):
-    """Showing how to search for mesh object that returns a time series."""
+def search_method_1(session: Connection.Session):
+    """
+    This method uses `search_for_timeseries_attributes` function.
+    Wildcard search expression for attributes is not supported, meaning we
+    can't get all time series attributes using simply "*" search expression.
 
-    # Configure the connection you want
-    connection = Connection(address, port, root_pem_certificate)
+    Useful e.g.: when searching for attributes with known names.
+    """
+    print("Search method 1")
 
-    # Create a remote session on the Volue Mesh server
-    session = connection.create_session()
-    session.open()
-
-    ###
     # Specify what you want to search for
-    ###
 
     # The Mesh object to start searching from
     start_object_path = "Model/SimpleThermalTestModel/ThermalComponent"
@@ -32,9 +30,81 @@ def main(address, port, root_pem_certificate):
     )
 
     print(f"Number of found time series: {len(timeseries_attributes)}")
+    for attribute in timeseries_attributes:
+        if isinstance(attribute, TimeseriesAttribute):
+            print(attribute.path)
 
-    # Close the remote session
-    session.close()
+
+def search_method_2(session: Connection.Session):
+    """
+    This method uses `search_for_objects` function with wildcard search
+    expression ("*") that returns all objects.
+    Useful when you want to traverse the complete model.
+
+    Additionally here we show how to distinguish if time series attribute has
+    a time series calculation expression defined or a physical time series
+    connected to it.
+    """
+    print("Search method 2")
+
+    # Provide root object/model as the start object to run the search.
+    # Root objects/models do not contain any attributes.
+    start_object_path = "Model/SimpleThermalTestModel"
+
+    # Returns every object.
+    query = "*"
+
+    # Depending on the model size this may take long time to complete.
+    objects = session.search_for_objects(target=start_object_path, query=query)
+
+    print(f"Number of found objects: {len(objects)}")
+    for object in objects:
+        print(object.name)
+        for attribute in object.attributes.values():
+            if isinstance(attribute, TimeseriesAttribute):
+                # If time series resource is set, then it means a physical
+                # time series is connected to the attribute.
+                if attribute.time_series_resource is not None:
+                    print(
+                        f"{attribute.path} has physical time series connected with time series key {attribute.time_series_resource.timeseries_key}"
+                    )
+
+
+def search_method_3(session: Connection.Session):
+    """
+    This method uses `get_object` function.
+    Useful when you want to traverse a subset of the model from a given node,
+    but not root object/model. Models do not contain attributes, so you won't
+    be able to traverse the child objects using ownership attributes.
+    """
+    print("Search method 3")
+
+    def traverse_child_objects(session: Connection.Session, target):
+        object = session.get_object(target, full_attribute_info=True)
+
+        for attribute in object.attributes.values():
+            if isinstance(attribute, OwnershipRelationAttribute):
+                for child_id in attribute.target_object_ids:
+                    traverse_child_objects(session, child_id)
+
+            if isinstance(attribute, TimeseriesAttribute):
+                print(attribute.path)
+
+    object_path = "Model/SimpleThermalTestModel/ThermalComponent/SomePowerPlant1"
+    traverse_child_objects(session, object_path)
+
+
+def main(address, port, root_pem_certificate):
+    """Showing how to search for Mesh time series attributes in various ways."""
+
+    # Configure the connection you want.
+    connection = Connection(address, port, root_pem_certificate)
+
+    # Create a remote session on the Volue Mesh server.
+    with connection.create_session() as session:
+        search_method_1(session)
+        search_method_2(session)
+        search_method_3(session)
 
 
 if __name__ == "__main__":
