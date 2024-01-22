168ec15b2dec36b55188444ac860a50e6caa0b39
Erik Nyhus <erik.nyhus@volue.com>
2024-01-22 12:32:26 +0100

    Use `CaseGroup/CaseName` to refer to hydsim cases
    `CaseGroup/CaseName` is established syntax used in various UIs, Optimal
    Gateway, and Automation Framework. As discussed in
    https://github.com/Volue/energy-sim/issues/742#issuecomment-1811942095.
    
    There's an argument for more user friendly error handling here, but I
    think this is API compatible with a future improvement, and I want to
    get this API break done ASAP. For now you'll get a `ValueError` if the
    split doesn't split.
    
    Closes https://github.com/Volue-Public/energy-mesh-python/issues/398.

--- src/volue/mesh/examples/run_simulation.py
+++ src/volue/mesh/examples/run_simulation.py
@@ -16,7 +16,7 @@ def main(address, port, root_pem_certificate):
 
         try:
             for response in session.run_simulation(
-                "Mesh", "Cases", "Demo", start_time, end_time, None, 0, False
+                "Mesh", "Cases/Demo", start_time, end_time, None, 0, False
             ):
                 pass
             print("done")
