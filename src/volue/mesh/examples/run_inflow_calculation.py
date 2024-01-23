6e05a85df0f8e47aae43ba4d543bafb58bd96f77
Erik Nyhus <erik.nyhus@volue.com>
2024-01-23 06:46:52 +0100

    Merge async and sync HydSim examples
    See https://github.com/Volue-Public/energy-mesh-python/pull/400#discussion_r1462025826.

--- src/volue/mesh/examples/run_inflow_calculation.py
+++ src/volue/mesh/examples/run_inflow_calculation.py
@@ -1,10 +1,13 @@
+import asyncio
 from datetime import datetime
 
 from volue import mesh
+import volue.mesh.aio
+
 from volue.mesh.examples import _get_connection_info
 
 
-def main(address, port, root_pem_certificate):
+def sync_run_inflow_calculation(address, port, root_pem_certificate):
     print("connecting...")
     connection = mesh.Connection(address, port, root_pem_certificate)
 
@@ -24,6 +27,27 @@ def main(address, port, root_pem_certificate):
             print(f"failed to run inflow calculation: {e}")
 
 
+async def async_run_inflow_calculation(address, port, root_pem_certificate):
+    print("connecting...")
+    connection = mesh.aio.Connection(address, port, root_pem_certificate)
+
+    async with connection.create_session() as session:
+        start_time = datetime(2021, 1, 1)
+        end_time = datetime(2021, 1, 2)
+
+        print("running inflow calculation...")
+
+        try:
+            async for response in session.run_inflow_calculation(
+                "Mesh", "Area", "WaterCourse", start_time, end_time
+            ):
+                pass
+            print("done")
+        except Exception as e:
+            print(f"failed to run inflow calculation: {e}")
+
+
 if __name__ == "__main__":
     address, port, root_pem_certificate = _get_connection_info()
-    main(address, port, root_pem_certificate)
+    sync_run_inflow_calculation(address, port, root_pem_certificate)
+    asyncio.run(async_run_inflow_calculation(address, port, root_pem_certificate))
