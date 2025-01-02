934844ded0ac33ae35e2570b52a84e5b028217ef
Martin Galvan <martin.galvan@volue.com>
2025-01-02 16:04:52 +0100

    .

--- src/volue/mesh/test_import_export.py
+++ src/volue/mesh/test_import_export.py
@@ -2,32 +2,44 @@ import threading
 import time
 import subprocess
 
-def main():
-    mesh_exe = ['C:/Users/martin.galvan/Documents/energy-mesh/Mesh/build/Debug/Powel.Mesh.Server.exe']
+from volue import mesh
 
-    with subprocess.Popen(mesh_exe) as mesh_proc:
-        imp_exp_exe = 'C:/Users/martin.galvan/Documents/energy-mesh/Mesh/build/Debug/Powel.Mesh.Model.ImportExport.exe'
-        imp_exp_args = [imp_exp_exe, '-i', 'base_dump.mdump', '-S']
+def main():
+    connection = mesh.Connection.insecure('localhost:50051')
+
+    mesh_build_path = 'C:/Users/martin.galvan/Documents/energy-mesh/Mesh/build/Debug'
+    mesh_exe = f'{mesh_build_path}/Powel.Mesh.Server.exe'
+    imp_exp_exe = f'{mesh_build_path}/Powel.Mesh.Model.ImportExport.exe'
+    dump_with_validity_path = 'with_validity.mdump'
+
+    with subprocess.Popen([mesh_exe]) as mesh_proc:
+        imp_args = [imp_exp_exe, '-i', 'C:/Users/martin.galvan/base_dump.mdump', '-S']
 
         # Import the base data first
-        subprocess.check_call(imp_exp_args)
+        subprocess.check_call(imp_args)
 
         # Set validity for an object (Models->MeshTEK->Mesh->To_Areas->Finland)
+        with connection.create_session() as session:
+            # set_validity()
+            session.commit()
 
-        # start_session()
-        # set_validity()
-        # commit()
-        # end_session()
-        # export_data_with_validity()
+        exp_args = [imp_exp_exe, dump_with_validity_path, '-m', 'MeshTEK']
+
+        # Export data with validity
+        subprocess.check_call(exp_args)
 
         mesh_proc.terminate()
 
     with subprocess.Popen(mesh_exe) as mesh_proc:
-        # start_session()
-        # import_data_with_validity()
-        # get_validity()
-        # end_session()
-        # check_validity()
+        imp_args = [imp_exp_exe, '-i', dump_with_validity_path, '-S']
+
+        # Import the base data first
+        subprocess.check_call(imp_args)
+
+        with connection.create_session() as session:
+            # get_validity()
+
+    # check_validity()
 
 if __name__ == '__main__':
     main()
