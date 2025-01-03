0442c316ec40491ad7c2dd00bffe3d0d5f662e7e
Martin Galvan <martin.galvan@volue.com>
2025-01-03 16:32:52 +0100

    .

--- src/volue/mesh/test_import_export.py
+++ src/volue/mesh/test_import_export.py
@@ -16,7 +16,7 @@ def main():
     imp_exp_exe = f"{mesh_build_path}/Powel.Mesh.Model.ImportExport.exe"
     dump_with_validity_path = "C:/Users/martin.galvan/with_validity.mdump"
 
-    print('[MARTIN] Starting mesh...')
+    print("[MARTIN] Starting mesh...")
 
     with subprocess.Popen([mesh_exe]) as mesh_proc:
         # Give mesh some time to finish starting up
@@ -24,11 +24,11 @@ def main():
 
         generate_data_with_validity(connection, imp_exp_exe, dump_with_validity_path)
 
-        print('[MARTIN] Terminating mesh...')
+        print("[MARTIN] Terminating mesh...")
 
         mesh_proc.terminate()
 
-    print('[MARTIN] Starting mesh...')
+    print("[MARTIN] Starting mesh...")
 
     with subprocess.Popen(mesh_exe) as mesh_proc:
         # Give mesh some time to finish starting up
@@ -36,20 +36,20 @@ def main():
 
         import_and_check_validity(connection, imp_exp_exe, dump_with_validity_path)
 
-        print('[MARTIN] Terminating mesh...')
+        print("[MARTIN] Terminating mesh...")
 
         mesh_proc.terminate()
 
     # check_validity()
 
 
-def generate_data_with_validity(connection: mesh.Connection,
-                                imp_exp_exe: str,
-                                dump_with_validity_path: str):
+def generate_data_with_validity(
+    connection: mesh.Connection, imp_exp_exe: str, dump_with_validity_path: str
+):
     imp_args = [imp_exp_exe, "-i", "C:/Users/martin.galvan/base_dump.mdump", "-S"]
 
     # Import the base data first
-    print('[MARTIN] Importing base data...')
+    print("[MARTIN] Importing base data...")
 
     subprocess.check_call(imp_args)
 
@@ -59,21 +59,21 @@ def generate_data_with_validity(connection: mesh.Connection,
 
         session.commit()
 
-    exp_args = [imp_exp_exe, '-o', dump_with_validity_path, "-m", "MeshTEK"]
+    exp_args = [imp_exp_exe, "-o", dump_with_validity_path, "-m", "MeshTEK"]
 
     # Export data with validity
-    print('[MARTIN] Exporting data with validity...')
+    print("[MARTIN] Exporting data with validity...")
 
     subprocess.check_call(exp_args)
 
 
-def import_and_check_validity(connection: mesh.Connection,
-                              imp_exp_exe: str,
-                              dump_with_validity_path: str):
+def import_and_check_validity(
+    connection: mesh.Connection, imp_exp_exe: str, dump_with_validity_path: str
+):
     imp_args = [imp_exp_exe, "-i", dump_with_validity_path, "-S"]
 
     # Import the data with validity
-    print('[MARTIN] Importing data with validity...')
+    print("[MARTIN] Importing data with validity...")
 
     subprocess.check_call(imp_args)
 
@@ -85,28 +85,26 @@ def import_and_check_validity(connection: mesh.Connection,
 def set_validity(session):
     # Llamar a UpdateValidity de protobuf
 
-    valid_from_datetime = datetime.fromisoformat('2024-12-04T00:00:00.000Z')
+    valid_from_datetime = datetime.fromisoformat("2024-12-04T00:00:00.000Z")
     valid_from = timestamp_pb2.Timestamp()
     valid_from.FromDatetime(valid_from_datetime)
 
-    valid_until_datetime = datetime.fromisoformat('2024-12-27T00:00:00.000Z')
+    valid_until_datetime = datetime.fromisoformat("2024-12-27T00:00:00.000Z")
     valid_until = timestamp_pb2.Timestamp()
     valid_until.FromDatetime(valid_until_datetime)
 
-    object_id = uuid.UUID('{21893300-6482-4b09-b9ba-58b48740d0e7}')
+    object_id = uuid.UUID("{21893300-6482-4b09-b9ba-58b48740d0e7}")
 
     request = time_series_pb2.UpdateValidityRequest(
         session_id=_to_proto_guid(session_id.session_id),
         object_id=_to_proto_guid(object_id),
         valid_from=valid_from,
-        valid_until=valid_until
+        valid_until=valid_until,
     )
 
     response = session.model_service.UpdateValidity(request)
 
     print(response)
-
-
 
     # unit_of_measurement_id = self._get_unit_of_measurement_id_by_name(
     #     unit_of_measurement
