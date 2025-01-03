a2f4cf4b4d476e0dbf0d80242aa4dfeaf7c09d7f
Martin Galvan <martin.galvan@volue.com>
2025-01-03 11:16:58 +0100

    .

--- src/volue/mesh/test_import_export.py
+++ src/volue/mesh/test_import_export.py
@@ -19,24 +19,7 @@ def main():
         # Give mesh some time to finish starting up
         time.sleep(10)
 
-        imp_args = [imp_exp_exe, "-i", "C:/Users/martin.galvan/base_dump.mdump", "-S"]
-
-        # Import the base data first
-        print('[MARTIN] Importing base data...')
-
-        subprocess.check_call(imp_args)
-
-        # Set validity for an object (Models->MeshTEK->Mesh->To_Areas->Finland)
-        with connection.create_session() as session:
-            # set_validity()
-            session.commit()
-
-        exp_args = [imp_exp_exe, '-o', dump_with_validity_path, "-m", "MeshTEK"]
-
-        # Export data with validity
-        print('[MARTIN] Exporting data with validity...')
-
-        subprocess.check_call(exp_args)
+        generate_data_with_validity(connection, imp_exp_exe, dump_with_validity_path)
 
         print('[MARTIN] Terminating mesh...')
 
@@ -48,22 +31,79 @@ def main():
         # Give mesh some time to finish starting up
         time.sleep(10)
 
-        imp_args = [imp_exp_exe, "-i", dump_with_validity_path, "-S"]
-
-        # Import the data with validity
-        print('[MARTIN] Importing data with validity...')
-
-        subprocess.check_call(imp_args)
-
-        with connection.create_session() as session:
-            pass
-            # get_validity()
+        import_and_check_validity(connection, imp_exp_exe, dump_with_validity_path)
 
         print('[MARTIN] Terminating mesh...')
 
         mesh_proc.terminate()
 
     # check_validity()
+
+
+def generate_data_with_validity(connection: mesh.Connection,
+                                imp_exp_exe: str,
+                                dump_with_validity_path: str):
+    imp_args = [imp_exp_exe, "-i", "C:/Users/martin.galvan/base_dump.mdump", "-S"]
+
+    # Import the base data first
+    print('[MARTIN] Importing base data...')
+
+    subprocess.check_call(imp_args)
+
+    # Set validity for an object (Models->MeshTEK->Mesh->To_Areas->Finland)
+    with connection.create_session() as session:
+        # set_validity()
+        session.commit()
+
+    exp_args = [imp_exp_exe, '-o', dump_with_validity_path, "-m", "MeshTEK"]
+
+    # Export data with validity
+    print('[MARTIN] Exporting data with validity...')
+
+    subprocess.check_call(exp_args)
+
+
+def import_and_check_validity(connection: mesh.Connection,
+                              imp_exp_exe: str,
+                              dump_with_validity_path: str):
+    imp_args = [imp_exp_exe, "-i", dump_with_validity_path, "-S"]
+
+    # Import the data with validity
+    print('[MARTIN] Importing data with validity...')
+
+    subprocess.check_call(imp_args)
+
+    with connection.create_session() as session:
+        pass
+        # get_validity()
+
+
+def set_validity():
+    # Llamar a UpdateValidity de protobuf
+
+    request = time_series_pb2.UpdateValidityRequest()
+
+    response = self.time_series_service.UpdateValidity(request)
+
+
+
+
+    # unit_of_measurement_id = self._get_unit_of_measurement_id_by_name(
+    #     unit_of_measurement
+    # )
+
+    # request = time_series_pb2.CreatePhysicalTimeseriesRequest(
+    #     session_id=_to_proto_guid(self.session_id),
+    #     path=path,
+    #     name=name,
+    #     curve_type=_to_proto_curve_type(curve_type),
+    #     resolution=_to_proto_resolution(resolution),
+    #     unit_of_measurement_id=unit_of_measurement_id,
+    # )
+
+    # response = self.time_series_service.CreatePhysicalTimeseries(request)
+
+    # return TimeseriesResource._from_proto_timeseries_resource(response)
 
 
 if __name__ == "__main__":
