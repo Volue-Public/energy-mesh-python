e01d5fc69f45e55c6de9c59b3a6323e1496eb286
Martin Galvan <martin.galvan@volue.com>
2025-01-07 16:22:45 +0100

    .

--- src/volue/mesh/tests/test_validity_import_export.py
+++ src/volue/mesh/tests/test_validity_import_export.py
@@ -4,6 +4,7 @@ import time
 import subprocess
 import uuid
 
+from collections.abc import Callable
 from datetime import datetime, timedelta
 
 from google.protobuf import timestamp_pb2
@@ -14,10 +15,9 @@ from volue.mesh import _common, _mesh_id
 from volue.mesh.proto import type
 from volue.mesh.proto.model.v1alpha import model_pb2
 
-BASE_DUMP_PATH = "C:/Users/martin.galvan/base_dump.mdump"
-DUMP_WITH_VALIDITY_PATH = "C:/Users/martin.galvan/with_validity.mdump"
+DUMPS_PATH = "C:/Users/martin.galvan"
+DUMP_WITH_VALIDITY_PATH = f"{DUMPS_PATH}/with_validity.mdump"
 MESH_BUILD_PATH = "C:/Users/martin.galvan/Documents/energy-mesh/Mesh/build/Debug"
-MESH_EXE = f"{MESH_BUILD_PATH}/Powel.Mesh.Server.exe"
 IMP_EXP_EXE = f"{MESH_BUILD_PATH}/Powel.Mesh.Model.ImportExport.exe"
 
 # FIXME: The checks seem to break if we use datetime.fromisoformat("2024-12-04T00:00:00.000Z") since
@@ -25,7 +25,7 @@ IMP_EXP_EXE = f"{MESH_BUILD_PATH}/Powel.Mesh.Model.ImportExport.exe"
 FROM_DATE = datetime.fromisoformat("2024-12-04T00:00:00")
 UNTIL_DATE = FROM_DATE + timedelta(days=1)
 
-class ValidityInfo:
+class ValidityInterval:
     def __init__(self, valid_from: datetime=None, valid_until: datetime=None):
         self.valid_from = valid_from
         self.valid_until = valid_until
@@ -34,7 +34,7 @@ class ValidityInfo:
         return f"valid_from: '{self.valid_from}'; valid_until: '{self.valid_until}'"
 
     def __eq__(self, other):
-        if isinstance(other, ValidityInfo):
+        if isinstance(other, ValidityInterval):
             return (
                 self.valid_from == other.valid_from
                 and self.valid_until == other.valid_until
@@ -75,51 +75,48 @@ class ValidityInfo:
 # Note that the target objects must be of type Model, Component, or AttributeElement.
 VALIDITY_TEST_DATA = {
     # Models->MeshTEK->Mesh->To_Areas->Finland
-    uuid.UUID("{21893300-6482-4b09-b9ba-58b48740d0e7}"): ValidityInfo(FROM_DATE, UNTIL_DATE),
+    uuid.UUID("{21893300-6482-4b09-b9ba-58b48740d0e7}"): ValidityInterval(FROM_DATE, UNTIL_DATE),
     # Models->MeshTEK->Mesh->To_Areas->Norge
-    uuid.UUID("{f06e67ed-61a6-4700-ac40-df80d752aeba}"): ValidityInfo(FROM_DATE, None),
+    uuid.UUID("{f06e67ed-61a6-4700-ac40-df80d752aeba}"): ValidityInterval(FROM_DATE, None),
     # Models->MeshTEK->Mesh->has_Market->Market->has_EnergyMarketFlows->NO1
-    uuid.UUID("{478f13d3-6e40-4db7-ae5b-3544ce04c546}"): ValidityInfo(None, UNTIL_DATE),
+    uuid.UUID("{478f13d3-6e40-4db7-ae5b-3544ce04c546}"): ValidityInterval(None, UNTIL_DATE),
 }
 
 class TestValidityImportExport:
     def test_set_validity(self, connection: mesh.Connection):
+        imported_validity_data = {}
+
+        self._run_mesh_and_do(self._generate_and_export_data_with_validity, connection)
+
+        self._run_mesh_and_do(self._import_data_with_validity, connection, imported_validity_data)
+
+        assert imported_validity_data == VALIDITY_TEST_DATA
+
+    def _run_mesh_and_do(callback: Callable, *args):
+        mesh_exe = f"{MESH_BUILD_PATH}/Powel.Mesh.Server.exe"
+
         print("[MARTIN] Starting mesh...")
 
-        mesh_proc = subprocess.Popen([MESH_EXE])
+        mesh_proc = subprocess.Popen([mesh_exe])
 
-        # We need to use try/finally instead of 'with subprocess.Popen(...)' since for some reason we
-        # won't be able to catch any exceptions until we terminate the mesh process.
+        # We need to use try/finally instead of 'with subprocess.Popen(...)' since for some reason
+        # we won't be able to catch any exceptions until we terminate the mesh process.
         try:
             # Give mesh some time to finish starting up
             time.sleep(10)
 
-            self._generate_data_with_validity(connection)
+            callback(*args)
         finally:
             print("[MARTIN] Terminating mesh...")
 
             mesh_proc.terminate()
 
-        print("[MARTIN] Starting mesh...")
-
-        mesh_proc = subprocess.Popen([MESH_EXE])
-
-        try:
-            # Give mesh some time to finish starting up
-            time.sleep(10)
-
-            imported_validity_info = self._import_and_get_validity(connection)
-        finally:
-            print("[MARTIN] Terminating mesh...")
-
-            mesh_proc.terminate()
-
-        assert imported_validity_info == VALIDITY_TEST_DATA
-
-    def _generate_data_with_validity(
+    def _generate_and_export_data_with_validity(
         self, connection: mesh.Connection
     ):
-        imp_args = [IMP_EXP_EXE, "-i", BASE_DUMP_PATH, "-S"]
+        base_dump_path = f"{DUMPS_PATH}/base_dump.mdump"
+
+        imp_args = [IMP_EXP_EXE, "-i", base_dump_path, "-S"]
 
         # Import the base data first
         print("[MARTIN] Importing base data...")
@@ -132,7 +129,7 @@ class TestValidityImportExport:
                 # First, verify that the object doesn't have any validity info set.
                 response = self._get_validity(session, object_id)
 
-                # assert response.valid_from == None and response.valid_until == None
+                assert response.valid_from == None and response.valid_until == None
 
                 # Now, set the validity.
                 self._set_validity(session, object_id, validity_info)
@@ -146,8 +143,9 @@ class TestValidityImportExport:
 
         subprocess.check_call(exp_args, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
-    def _import_and_get_validity(self, connection: mesh.Connection) -> dict[uuid.UUID, ValidityInfo]:
-        result = {}
+    def _import_data_with_validity(self, connection: mesh.Connection, result: dict[uuid.UUID, ValidityInterval]):
+        result.clear()
+
         imp_args = [IMP_EXP_EXE, "-i", DUMP_WITH_VALIDITY_PATH, "-S"]
 
         # Import the data with validity.
@@ -164,7 +162,7 @@ class TestValidityImportExport:
 
         return result
 
-    def _set_validity(self, session: mesh.Connection.Session, object_id: uuid.UUID, validity_info: ValidityInfo):
+    def _set_validity(self, session: mesh.Connection.Session, object_id: uuid.UUID, validity_info: ValidityInterval):
         print("[MARTIN] Setting validity for object...")
 
         valid_from, valid_until = validity_info.to_proto_timestamps()
@@ -180,7 +178,7 @@ class TestValidityImportExport:
 
         print(f"[MARTIN] Done! Response: '{response}'")
 
-    def _get_validity(self, session: mesh.Connection.Session, object_id: uuid.UUID) -> ValidityInfo:
+    def _get_validity(self, session: mesh.Connection.Session, object_id: uuid.UUID) -> ValidityInterval:
         print("[MARTIN] Getting validity for object...")
 
         request = model_pb2.GetValidityRequest(
@@ -192,13 +190,13 @@ class TestValidityImportExport:
 
         print(f"[MARTIN] Done! Response: '{response}'")
 
-        # We need these explicit checks here since otherwise the pb response will return
-        # default values for the fields instead of None. In the case of timestamp_pb2, this
-        # is the Unix epoch. See also https://protobuf.dev/reference/python/python-generated/#embedded_message
+        # We need these explicit checks here since otherwise the pb response will return default
+        # values for the fields instead of None. In the case of timestamp_pb2, this is the Unix
+        # epoch. See also https://protobuf.dev/reference/python/python-generated/#embedded_message
         valid_from = response.valid_from if response.HasField('valid_from') else None
         valid_until = response.valid_until if response.HasField('valid_until') else None
 
-        validity_info = ValidityInfo()
+        validity_info = ValidityInterval()
         validity_info.from_proto_timestamps(valid_from, valid_until)
 
         return validity_info
