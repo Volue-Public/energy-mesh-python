1bf177ea485b3d547eaf5145cc1fe2d56bbf8cb5
Martin Galvan <martin.galvan@volue.com>
2025-06-19 16:08:18 +0200

    .

--- src/volue/mesh/examples/auction_bidding.py
+++ src/volue/mesh/examples/auction_bidding.py
@@ -1,18 +1,11 @@
-import argparse
 import os
 
 import helpers
 
-from volue.mesh import AttributesFilter, Connection
+from volue.mesh import AttributesFilter, Connection, SimpleAttribute, TimeseriesAttribute
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Example script for fixing up AuctionBidding models')
-
-    parser.add_argument('model_name', help='Name of the model to fix')
-
-    args = parser.parse_args()
-
     address, tls_root_pem_cert = helpers.get_connection_info()
 
     # For production environments create connection using: with_tls, with_kerberos, or with_external_access_token, e.g.:
@@ -20,11 +13,11 @@ def main():
     connection = Connection.insecure(address)
 
     with connection.create_session() as session:
-        fix_auction_bidding(args.model_name, session)
+        fix_auction_bidding(session)
 
 
-def fix_auction_bidding(model_name: str, session: Connection.Session):
-    target = f"Model/{model_name}"
+def fix_auction_bidding(session: Connection.Session):
+    target = "Model/DemoModel"
     query = "*[.Type=CurveOrders]"
     curve_orders_attribute_names = ["MinVolume", "MaxVolume", "VolumeMin", "VolumeMax"]
     curve_orders_attributes = AttributesFilter(name_mask=curve_orders_attribute_names)
@@ -40,29 +33,32 @@ def fix_auction_bidding(model_name: str, session: Connection.Session):
         # object, which is less efficient.
         parent_object_path = os.path.splitext(curve_orders_obj.owner_path)[0]
 
-        print(parent_object_path)
+        ownership_attribute_name = "has_AuctionOrders"
+        ownership_attribute = AttributesFilter(name_mask=[ownership_attribute_name])
 
-    #     ownership_attribute_name = "has_AuctionOrders"
-    #     ownership_attribute = AttributesFilter(name_mask=[ownership_attribute_name])
+        parent = session.get_object(parent_object_path, attributes_filter=ownership_attribute)
 
-    #     parent = session.get_object(parent_object_path, attributes_filter=ownership_attribute)
+        # For each CurveOrders object, create a corresponding AuctionOrders object.
+        auction_orders_obj = session.create_object(parent.attributes[ownership_attribute_name],
+                                                   curve_orders_obj.name)
 
-    #     # For each CurveOrders object, create a corresponding AuctionOrders object.
-    #     auction_orders_obj = session.create_object(parent.attributes[ownership_attribute_name],
-    #                                                curve_orders_obj.name)
+        # Finally, set the values of the new AuctionOrders object to those of the corresponding
+        # CurveOrders.
+        for attribute_name in curve_orders_attribute_names:
+            curve_orders_attribute = curve_orders_obj.attributes[attribute_name]
+            auction_orders_attribute = auction_orders_obj.attributes[attribute_name]
 
-    #     print(auction_orders_obj)
-
-    #     # Finally, set the values of the new AuctionOrders object to those of the corresponding
-    #     # CurveOrders.
-    #     for attribute_name in curve_orders_attribute_names:
-    #         curve_orders_attribute = curve_orders_obj.attributes[attribute_name]
-    #         auction_orders_attribute = auction_orders_obj.attributes[attribute_name]
-
-    #         print(curve_orders_attribute)
-    #         print(auction_orders_attribute)
-
-    #         session.update_simple_attribute(auction_orders_attribute, curve_orders_attribute.value)
+            # FIXME: What happens with all the extra fields from the newly-created objects which
+            # don't have a corresponding value in the CurveOrders attribute objects?
+            if isinstance(auction_orders_attribute, SimpleAttribute):
+                session.update_simple_attribute(auction_orders_attribute,
+                                                curve_orders_attribute.value)
+            elif isinstance(auction_orders_attribute, TimeseriesAttribute):
+                session.update_timeseries_attribute(auction_orders_attribute,
+                                                    curve_orders_attribute.expression,
+                                                    curve_orders_attribute.time_series_resource.timeseries_key)
+            else:
+                raise TypeError(f"Unexpected type of attribute '{type(auction_orders_attribute)}'")
 
     # session.commit()
 
