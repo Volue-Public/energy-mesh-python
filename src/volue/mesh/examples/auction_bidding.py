93c285fccfee15ba4745c6bdc1a6374479b557cf
Martin Galvan <martin.galvan@volue.com>
2025-06-19 14:32:04 +0200

    .

--- src/volue/mesh/examples/auction_bidding.py
+++ src/volue/mesh/examples/auction_bidding.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 import helpers
@@ -5,21 +6,33 @@ import helpers
 from volue.mesh import AttributesFilter, Connection
 
 
-def main(address):
+def main():
+    parser = argparse.ArgumentParser(description='Example script for fixing up AuctionBidding models')
+
+    parser.add_argument('model_name', help='Name of the model to fix')
+
+    args = parser.parse_args()
+
+    address, tls_root_pem_cert = helpers.get_connection_info()
+
+    # For production environments create connection using: with_tls, with_kerberos, or with_external_access_token, e.g.:
+    # connection = Connection.with_tls(address, tls_root_pem_cert)
     connection = Connection.insecure(address)
 
     with connection.create_session() as session:
-        fix_auction_bidding(session)
+        fix_auction_bidding(args.model_name, session)
 
 
-def fix_auction_bidding(session: Connection.Session):
-    target = "Model/POMAtest01" # Something based off EnergySystem
+def fix_auction_bidding(model_name: str, session: Connection.Session):
+    target = f"Model/{model_name}"
     query = "*[.Type=CurveOrders]"
     curve_orders_attribute_names = ["MinVolume", "MaxVolume", "VolumeMin", "VolumeMax"]
     curve_orders_attributes = AttributesFilter(name_mask=curve_orders_attribute_names)
 
     # First, get all the CurveOrders objects in the model.
-    for curve_orders_obj in session.search_for_objects(target, query, attributes_filter=curve_orders_attributes):
+    for curve_orders_obj in session.search_for_objects(target,
+                                                       query,
+                                                       attributes_filter=curve_orders_attributes):
         # This is a bit ugly, since we're relying on the fact that objects' attributes are specified
         # with a dot in their path strings. The most "correct" way of grabbing the object's parent
         # (as opposed to the attribute which serves as its owner) would be to query Mesh using
@@ -29,30 +42,30 @@ def fix_auction_bidding(session: Connection.Session):
 
         print(parent_object_path)
 
-        ownership_attribute_name = "has_AuctionOrders"
-        ownership_attribute = AttributesFilter(name_mask=[ownership_attribute_name])
+    #     ownership_attribute_name = "has_AuctionOrders"
+    #     ownership_attribute = AttributesFilter(name_mask=[ownership_attribute_name])
 
-        parent = session.get_object(parent_object_path, attributes_filter=ownership_attribute)
+    #     parent = session.get_object(parent_object_path, attributes_filter=ownership_attribute)
 
-        # For each CurveOrders object, create a corresponding AuctionOrders object.
-        auction_orders_obj = session.create_object(parent.attributes[ownership_attribute_name], curve_orders_obj.name)
+    #     # For each CurveOrders object, create a corresponding AuctionOrders object.
+    #     auction_orders_obj = session.create_object(parent.attributes[ownership_attribute_name],
+    #                                                curve_orders_obj.name)
 
-        print(auction_orders_obj)
+    #     print(auction_orders_obj)
 
-        # Set the values of the new AuctionOrders object to those of the corresponding CurveOrders.
-        for attribute_name in curve_orders_attribute_names:
-            curve_orders_attribute = curve_orders_obj.attributes[attribute_name]
-            auction_orders_attribute = auction_orders_obj.attributes[attribute_name]
+    #     # Finally, set the values of the new AuctionOrders object to those of the corresponding
+    #     # CurveOrders.
+    #     for attribute_name in curve_orders_attribute_names:
+    #         curve_orders_attribute = curve_orders_obj.attributes[attribute_name]
+    #         auction_orders_attribute = auction_orders_obj.attributes[attribute_name]
 
-            print(curve_orders_attribute)
-            print(auction_orders_attribute)
+    #         print(curve_orders_attribute)
+    #         print(auction_orders_attribute)
 
-            session.update_simple_attribute(auction_orders_attribute, curve_orders_attribute.value)
+    #         session.update_simple_attribute(auction_orders_attribute, curve_orders_attribute.value)
 
-        session.commit()
+    # session.commit()
 
 
 if __name__ == "__main__":
-    address, _ = helpers.get_connection_info()
-
-    main(address)
+    main()
