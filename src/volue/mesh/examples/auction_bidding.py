f560af5781fa8bb0f4ed25306206c0f3204739b5
Martin Galvan <martin.galvan@volue.com>
2025-06-20 14:42:10 +0200

    .

--- src/volue/mesh/examples/auction_bidding.py
+++ src/volue/mesh/examples/auction_bidding.py
@@ -2,7 +2,12 @@ import os
 
 import helpers
 
-from volue.mesh import AttributesFilter, Connection, Object, SimpleAttribute, TimeseriesAttribute
+from volue.mesh import (AttributesFilter,
+                        Connection,
+                        Object,
+                        SimpleAttribute,
+                        TimeseriesAttribute,
+                        VersionedLinkRelationAttribute)
 
 
 def main():
@@ -19,7 +24,7 @@ def main():
 def fix_auction_bidding(session: Connection.Session):
     target = "Model/DemoModel"
     query = "*[.Type=CurveOrders]"
-    curve_orders_attr_names = ["MinVolume", "MaxVolume", "VolumeMin", "VolumeMax"]
+    curve_orders_attr_names = ["MinVolume", "MaxVolume", "VolumeMin", "VolumeMax", "to_Areas"]
     curve_orders_attrs = AttributesFilter(name_mask=curve_orders_attr_names)
 
     # First, get all the CurveOrders objects in the model.
@@ -31,6 +36,8 @@ def fix_auction_bidding(session: Connection.Session):
         # (as opposed to the attribute which serves as its owner) would be to query Mesh using
         # 'get_attribute' to get the owner attribute and 'get_object' to get the attribute's owner
         # object, which is less efficient.
+        print(curve_orders_obj)
+
         parent_object_path = os.path.splitext(curve_orders_obj.owner_path)[0]
 
         ownership_attr_name = "has_AuctionOrders"
@@ -38,11 +45,14 @@ def fix_auction_bidding(session: Connection.Session):
 
         parent = session.get_object(parent_object_path, attributes_filter=ownership_attr)
 
-        # For each CurveOrders object, create a corresponding AuctionOrders object.
+        # For each CurveOrders object, create a corresponding AuctionOrders object and set it as the
+        # CurveOrders object's owner.
         auction_orders_obj = session.create_object(parent.attributes[ownership_attr_name],
                                                    curve_orders_obj.name)
 
-        print(f"Created object:\n{auction_orders_obj.path}\nfrom object:\n{curve_orders_obj.path}\n")
+        curve_orders_ownership_attr = auction_orders_obj.attributes["has_CurveOrders"]
+
+        session.update_object(curve_orders_obj.id, new_owner_attribute=curve_orders_ownership_attr)
 
         # Finally, set the values of the new AuctionOrders object to those of the corresponding
         # CurveOrders.
@@ -59,14 +69,20 @@ def copy_attribute_values(session: Connection.Session,
     curve_orders_attr = curve_orders_obj.attributes[attr_name]
     auction_orders_attr = auction_orders_obj.attributes[attr_name]
 
+    print(curve_orders_attr)
+    print(auction_orders_attr)
+
     if isinstance(auction_orders_attr, SimpleAttribute):
         session.update_simple_attribute(auction_orders_attr, curve_orders_attr.value)
     elif isinstance(auction_orders_attr, TimeseriesAttribute):
         session.update_timeseries_attribute(auction_orders_attr,
                                             curve_orders_attr.expression,
                                             curve_orders_attr.time_series_resource.timeseries_key)
+    elif isinstance(auction_orders_attr, VersionedLinkRelationAttribute):
+        session.update_versioned_one_to_many_link_relation_attribute(auction_orders_attr,
+                                                                     curve_orders_attr.entries)
     else:
-        raise TypeError(f"Unexpected type of attribute '{type(auction_orders_attr)}'")
+        raise TypeError(f"Found attribute '{attr_name}' of unexpected type '{type(auction_orders_attr)}'")
 
 
 if __name__ == "__main__":
