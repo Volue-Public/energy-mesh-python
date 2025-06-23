1683ad92ec3c32e07817cbec267ef081d12771cf
Martin Galvan <martin.galvan@volue.com>
2025-06-23 13:02:03 +0200

    .

--- src/volue/mesh/examples/auction_bidding.py
+++ src/volue/mesh/examples/auction_bidding.py
@@ -1,13 +1,18 @@
 import os
+import uuid
+
+from datetime import datetime
+from typing import List
 
 import helpers
 
 from volue.mesh import (AttributesFilter,
                         Connection,
+                        LinkRelationAttribute,
+                        LinkRelationVersion,
                         Object,
                         SimpleAttribute,
-                        TimeseriesAttribute,
-                        VersionedLinkRelationAttribute)
+                        TimeseriesAttribute)
 
 
 def main():
@@ -40,19 +45,19 @@ def fix_auction_bidding(session: Connection.Session):
 
         parent_object_path = os.path.splitext(curve_orders_obj.owner_path)[0]
 
-        ownership_attr_name = "has_AuctionOrders"
-        ownership_attr = AttributesFilter(name_mask=[ownership_attr_name])
+        has_auction_orders = "has_AuctionOrders"
+        has_auction_orders_attr = AttributesFilter(name_mask=[has_auction_orders])
 
-        parent = session.get_object(parent_object_path, attributes_filter=ownership_attr)
+        parent = session.get_object(parent_object_path, attributes_filter=has_auction_orders_attr)
 
         # For each CurveOrders object, create a corresponding AuctionOrders object and set it as the
         # CurveOrders object's owner.
-        auction_orders_obj = session.create_object(parent.attributes[ownership_attr_name],
+        auction_orders_obj = session.create_object(parent.attributes[has_auction_orders],
                                                    curve_orders_obj.name)
 
-        curve_orders_ownership_attr = auction_orders_obj.attributes["has_CurveOrders"]
+        has_curve_orders_attr = auction_orders_obj.attributes["has_CurveOrders"]
 
-        session.update_object(curve_orders_obj.id, new_owner_attribute=curve_orders_ownership_attr)
+        session.update_object(curve_orders_obj.id, new_owner_attribute=has_curve_orders_attr)
 
         # Finally, set the values of the new AuctionOrders object to those of the corresponding
         # CurveOrders.
@@ -78,16 +83,35 @@ def copy_attribute_values(session: Connection.Session,
         session.update_timeseries_attribute(auction_orders_attr,
                                             curve_orders_attr.expression,
                                             curve_orders_attr.time_series_resource.timeseries_key)
-    elif isinstance(auction_orders_attr, VersionedLinkRelationAttribute):
-        # to_Areas should be a one-to-one link relation.
-        assert(len(curve_orders_attr.entries) == 0)
+    elif isinstance(auction_orders_attr, LinkRelationAttribute):
+        if curve_orders_attr.entries:
+            # to_Areas should be a one-to-one link relation.
+            assert len(curve_orders_attr.entries) == 1
 
-        session.update_versioned_one_to_one_link_relation_attribute(auction_orders_attr,
-                                                                    datetime.min,
-                                                                    datetime.max,
-                                                                    curve_orders_attr.entries)
+            versions = curve_orders_attr.entries[0].versions
+
+            if versions:
+                target_object_id = find_current_target_object_id(versions)
+                ids = [target_object_id] if target_object_id is not None else []
+
+                session.update_link_relation_attribute(auction_orders_attr, ids)
     else:
         raise TypeError(f"Found attribute '{attr_name}' of unexpected type '{type(auction_orders_attr)}'")
+
+
+# AuctionOrders.to_Areas is defined as a LinkRelationAttribute (i.e. ReferenceAttributeDefinition),
+# whereas CurverOrders.to_Areas is a VersionedLinkRelationAttribute
+# (i.e. ReferenceSeriesAttributeDefinition). In cases where CurveOrders.to_Areas has multiple
+# versions (i.e. it points to different objects throughout time), we'll take the one corresponding
+# to the current system time. Note that this assumes the system time is properly configured in the
+# environment we're running this script on.
+def find_current_target_object_id(versions: List[LinkRelationVersion]) -> uuid.UUID:
+    tzinfo = versions[0].valid_from_time.tzinfo
+    current_time = datetime.now(tzinfo)
+
+    current_version = next(v for v in reversed(versions) if v.valid_from_time <= current_time)
+
+    return current_version.target_object_id
 
 
 if __name__ == "__main__":
