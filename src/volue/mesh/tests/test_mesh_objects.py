9098bd344481b7e109539ff52d826c38e19d2bd1
Tomasz Kaczkowski <tomasz.kaczkowski@volue.com>
2025-08-06 09:38:26 +0200

    Fix number of expected attributes in simple thermal model

--- src/volue/mesh/tests/test_mesh_objects.py
+++ src/volue/mesh/tests/test_mesh_objects.py
@@ -23,7 +23,7 @@ OBJECT_ID = uuid.UUID("0000000A-0001-0000-0000-000000000000")
 
 def verify_object_attributes(object: Object, full_info: bool = False):
     """Verifies all attributes of SomePowerPlant1 object."""
-    assert len(object.attributes) == 32
+    assert len(object.attributes) == 33
 
     for attribute in object.attributes.values():
         assert isinstance(attribute, AttributeBase)
